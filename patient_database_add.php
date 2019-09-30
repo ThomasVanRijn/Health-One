9cb42aaae50ce234acdf05d7ad9fa5fea7eab31b
TScollin <302983549@student.rocmondriaan.nl>
2019-09-30 13:32:22 +0200

    database add

---
+++ patient_database_add.php
@@ -0,0 +1,33 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=HealthOne", "root", "");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+// Attempt insert query execution
+try{
+    // Create prepared statement
+    $sql = "INSERT INTO patient (id, naam, adres ,email) VALUES (:id, :naam, :adres, :email);";
+    $stmt = $pdo->prepare($sql);
+
+    // Bind parameters to statement
+    $stmt->bindParam(':id', $_REQUEST['id']);
+    $stmt->bindParam(':naam', $_REQUEST['naam']);
+    $stmt->bindParam(':adres', $_REQUEST['adres']);
+    $stmt->bindParam(':email', $_REQUEST['email']);
+
+    // Execute the prepared statement
+    $stmt->execute();
+    echo "Records inserted successfully.";
+} catch(PDOException $e){
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+
+// Close connection
+unset($pdo);
+?>
