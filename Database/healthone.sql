6c86d94b1343f1c4957a391a8e8d8b003f47dcaa
ThomasVanRijn <302190448@student.rocmdondriaan.nl>
2019-10-14 10:11:37 +0200

    asdasd

--- Database/healthone.sql
+++ Database/healthone.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 08 okt 2019 om 11:46
+-- Gegenereerd op: 14 okt 2019 om 10:11
 -- Serverversie: 10.4.6-MariaDB
--- PHP-versie: 7.3.9
+-- PHP-versie: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,23 +27,46 @@ USE `healthone`;
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `artsen`
+--
+
+CREATE TABLE `artsen` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adres` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefoonnummer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `artsen`
+--
+
+INSERT INTO `artsen` (`id`, `naam`, `adres`, `email`, `telefoonnummer`) VALUES
+(1, 'Thomas van Rijn', 'Straat 1', 'thomas@gmail.com', 636356592),
+(2, 'Luca van Leeuwen', 'Straat 2', 'luca@gmail.com', 623545812),
+(3, 'Collin van de Laar', 'Straat 3', 'collin@gmail.com', 659874581),
+(4, 'Gijs de Lange', 'Straat 4', 'gijs@gmail.com', 621547852);
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `medicijnen`
 --
 
 CREATE TABLE `medicijnen` (
   `id` int(255) NOT NULL,
   `naam` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `herhaal` tinyint(1) NOT NULL,
-  `vergoed` tinyint(1) NOT NULL
+  `vergoed` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `medicijnen`
 --
 
-INSERT INTO `medicijnen` (`id`, `naam`, `herhaal`, `vergoed`) VALUES
-(2, 'aids pil', 1, 0),
-(3, 'naampje', 0, 1);
+INSERT INTO `medicijnen` (`id`, `naam`, `vergoed`) VALUES
+(13, 'Paracetamol', 'nee'),
+(16, 'Antibiotica', 'nee');
 
 -- --------------------------------------------------------
 
@@ -53,6 +76,8 @@ INSERT INTO `medicijnen` (`id`, `naam`, `herhaal`, `vergoed`) VALUES
 
 CREATE TABLE `patient` (
   `id` int(255) NOT NULL,
+  `medicijn_id` int(11) NOT NULL,
+  `recept_id` int(11) NOT NULL,
   `naam` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `leeftijd` int(255) NOT NULL,
   `adres` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -66,11 +91,11 @@ CREATE TABLE `patient` (
 -- Gegevens worden geëxporteerd voor tabel `patient`
 --
 
-INSERT INTO `patient` (`id`, `naam`, `leeftijd`, `adres`, `email`, `telefoonnummer`, `verzekeringsnummer`, `aandoeningen`) VALUES
-(1, 'Dinna de Waard', 17, 'Vogelkersstraat 3', 'daandata1@gmail.com', 64868148, 12985, 'Cholera'),
-(3, 'Dylanus van der Hout', 17, 'Boomaweg 6a', 'Dylanvanderhout@gmail.com', 657119062, 132434344, ''),
-(4, 'Collin van de Laar', 17, 'heulstraat 3', 'collinvandelaar@gmail.com', 657119061, 856, 'Geen'),
-(348, 'Marcel de Jong', 23, 'Lange Straat 11', 'marceldejong@gmail.com', 612895520, 1192, 'Stress');
+INSERT INTO `patient` (`id`, `medicijn_id`, `recept_id`, `naam`, `leeftijd`, `adres`, `email`, `telefoonnummer`, `verzekeringsnummer`, `aandoeningen`) VALUES
+(1, 0, 0, 'Dinna de Waard', 17, 'Vogelkersstraat 3', 'daandata1@gmail.com', 64868148, 12985, 'Cholera'),
+(3, 0, 0, 'Dylanus van der Hout', 17, 'Boomaweg 6a', 'Dylanvanderhout@gmail.com', 657119062, 132434344, ''),
+(4, 0, 0, 'Collin van de Laar', 17, 'heulstraat 3', 'collinvandelaar@gmail.com', 657119061, 856, 'Geen'),
+(348, 0, 0, 'Marcel de Jong', 23, 'Lange Straat 11', 'marceldejong@gmail.com', 612895520, 1192, 'Stress');
 
 -- --------------------------------------------------------
 
@@ -80,10 +105,21 @@ INSERT INTO `patient` (`id`, `naam`, `leeftijd`, `adres`, `email`, `telefoonnumm
 
 CREATE TABLE `recepten` (
   `id` int(11) NOT NULL,
-  `uitgeschreven` timestamp NOT NULL DEFAULT current_timestamp(),
-  `opgehaald` timestamp NOT NULL DEFAULT current_timestamp(),
-  `patient_id` int(11) NOT NULL
+  `patient_id` int(11) NOT NULL,
+  `medicijn_id` int(11) NOT NULL,
+  `herhaal` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dosering` int(11) NOT NULL,
+  `omschrijving` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uitgeschreven` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `recepten`
+--
+
+INSERT INTO `recepten` (`id`, `patient_id`, `medicijn_id`, `herhaal`, `dosering`, `omschrijving`, `uitgeschreven`) VALUES
+(75, 3, 13, 'ja', 7, '1 week elke avond', '2019-10-10 12:24:26'),
+(76, 1, 13, 'nee', 45, 'asdasdasd', '2019-10-14 06:51:17');
 
 -- --------------------------------------------------------
 
@@ -108,15 +144,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `functie`, `naam`, `adres`, `email`, `telefoonnummer`) VALUES
-(18, 'arts1', '$2y$10$4dHpPL9q7xSo/N0vVG8hhOu3jycf8DGJulcUp4zW4Kj5zlvpDs8SW', '2019-10-07 08:58:31', 'artsen', 'Pietje', 'straat1', 'asd@asd.com', 2147483647),
-(23, 'apo123', '$2y$10$0PZ.j.m90S61/fxAQknO.Og2A5p96J5aicnXNA6eam4bV3wle3wZS', '2019-10-07 19:46:21', 'apotheker', 'apo21341', '234324532', '423423', 2147483647),
-(24, 'verzekering', '$2y$10$yrNs9U5z06tO0rWljfSkAuumZFw2vSsgy20Aqf2IQ5vuj2hudeFF2', '2019-10-07 19:48:24', 'verzekering', 'verzekering', 'verzekering', 'verzekering@.com', 24322354),
-(25, 'artstest', '$2y$10$WNvtyfN.mb.2kcVmZIUL5ukEeFa9DVv2clVGh//BiL4/0./3B7sAS', '2019-10-07 19:55:18', 'artsen', 'artstest', 'artstest', 'arts@arts.com', 2342134),
-(26, 'arts1234', '$2y$10$GkNxu/BgjiEZKVRKTMrG8OEbA8ulp.vRn/zs9tOEZhM35PyKNzUnK', '2019-10-07 20:05:09', 'artsen', 'arts1234', 'arts1234', 'fsdf', 3434453);
+(17, 'test', '$2y$10$uoY02OwPMblodY.IR9bV4uHBSVLzHQKmVZSYihGJSJaYQXGDIyL.y', '2019-10-07 08:51:58', 'artsen', 'test', 'test', 'test', 989021),
+(18, 'arts1', '$2y$10$4dHpPL9q7xSo/N0vVG8hhOu3jycf8DGJulcUp4zW4Kj5zlvpDs8SW', '2019-10-07 08:58:31', 'artsen', 'Piet', 'straat1', 'asd@asd.com', 2147483647),
+(20, 'apotheker', '$2y$10$Hu9rK3QNH9xbpsSPsHZuQu3V7o.IJKf1o3fqym.MlQ6i6fayiHy3W', '2019-10-14 09:25:37', 'apotheker', 'jan', 'straat 2', 'jan@gmail.com', 656984512);
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `artsen`
+--
+ALTER TABLE `artsen`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `medicijnen`
@@ -128,13 +168,15 @@ ALTER TABLE `medicijnen`
 -- Indexen voor tabel `patient`
 --
 ALTER TABLE `patient`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `medicijn_id` (`medicijn_id`,`recept_id`);
 
 --
 -- Indexen voor tabel `recepten`
 --
 ALTER TABLE `recepten`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `medicijn_id` (`medicijn_id`),
   ADD KEY `patient_id` (`patient_id`);
 
 --
@@ -150,10 +192,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT voor een tabel `artsen`
+--
+ALTER TABLE `artsen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT voor een tabel `medicijnen`
 --
 ALTER TABLE `medicijnen`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT voor een tabel `patient`
@@ -165,13 +213,13 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT voor een tabel `recepten`
 --
 ALTER TABLE `recepten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
@@ -181,7 +229,8 @@ ALTER TABLE `users`
 -- Beperkingen voor tabel `recepten`
 --
 ALTER TABLE `recepten`
-  ADD CONSTRAINT `recepten_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`);
+  ADD CONSTRAINT `medicijn_id` FOREIGN KEY (`medicijn_id`) REFERENCES `medicijnen` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
